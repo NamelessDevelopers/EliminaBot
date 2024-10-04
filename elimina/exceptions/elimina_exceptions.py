57d5960c675385093767fc79254509e17c2adcec
ayamdobhal <me@iamdobhal.dev>
2024-10-04 21:09:55 +0530

    doc: added exceptions doc

--- elimina/exceptions/elimina_exceptions.py
+++ elimina/exceptions/elimina_exceptions.py
@@ -1,7 +1,15 @@
 class BaseEliminaException(Exception):
+    """
+    Base Class for Elimina Exceptions.
+    """
+
     pass
 
 
 class TimeValueError(BaseEliminaException):
+    """
+    Exception thrown when the `time` for the `timer` command is not in the 1-300s range
+    """
+
     def __init__(self, time: int) -> None:
         self.time = time
