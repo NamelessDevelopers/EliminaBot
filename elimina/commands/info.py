142ee25dffb094798b1719468ccec57a6bb63f3f
ayamdobhal <me@iamdobhal.dev>
2024-10-04 21:10:24 +0530

    refactor: info class

--- elimina/commands/info.py
+++ elimina/commands/info.py
@@ -2,9 +2,9 @@ import discord
 from discord import Embed
 from discord.ext import commands
 
-from elimina import LOGGER
+from elimina import LOGGER, config
 from elimina.constants import COLORS, SUPER_USERS
-from elimina.utils.fileHandler import BOT, DATA
+from elimina.db.guild import get_guild, get_whitelists
 
 
 class Info(commands.Cog):
@@ -13,182 +13,186 @@ class Info(commands.Cog):
 
     def __init__(self, bot: commands.Bot) -> None:
         self.bot = bot
+        self.bot_id = bot.user.id
 
     @commands.command(name="help")
     async def _help(self, ctx: commands.Context) -> None:
-        embedHelp = Embed(title="**Commands**", colour=COLORS["accent"])
+        help_embed = Embed(title="**Commands**", colour=COLORS["accent"])
 
-        embedHelp.add_field(
-            name="`~info` :",
+        help_embed.add_field(
+            name=f"`{config.BOT_PREFIX}info` :",
             value="  *To get information about how Elimina is setup in this server.*",
         )
-        embedHelp.add_field(
-            name="`~toggle` :  __(Admin only)__",
+        help_embed.add_field(
+            name=f"`{config.BOT_PREFIX}toggle` :  __(Admin only)__",
             value="  *Type in channel to activate or deactivate the bot in the respective channel.*",
         )
-        embedHelp.add_field(
-            name="`~ignore <bot>` : __(Admin only)__",
+        help_embed.add_field(
+            name=f"`{config.BOT_PREFIX}ignore <bot>` : __(Admin only)__",
             value="*Add or Remove the bot(s) from server's Elimina whitelist*",
         )
-        embedHelp.add_field(
-            name="`~timer <time in seconds>` :  __(Admin Only)__",
+        help_embed.add_field(
+            name=f"`{config.BOT_PREFIX}timer <time in seconds>` :  __(Admin Only)__",
             value="  *Change default timer after which the bot messages are deleted. Default: 15 seconds*",
         )
-        embedHelp.add_field(
-            name="`~purge` <number of messages>[optional]:",
+        help_embed.add_field(
+            name=f"`{config.BOT_PREFIX}purge` <number of messages>[optional]:",
             value="  *Type in channel to manually clear recent(or if specified, n number of) messages sent by bots*",
         )
-        embedHelp.add_field(
-            name="`~snipe` :",
+        help_embed.add_field(
+            name=f"`{config.BOT_PREFIX}snipe` :",
             value="*To show the last text message or image deleted by a user within a minute on the same channel. _Does not snipe bots._*",
         )
-        embedHelp.add_field(
-            name="`~imgsnipe` : __(Admin only)__",
+        help_embed.add_field(
+            name=f"`{config.BOT_PREFIX}imgsnipe` : __(Admin only)__",
             value="*To enable/disable sniping of images using the `snipe` command.*",
         )
-        embedHelp.add_field(
-            name="`~invite` :",
+        help_embed.add_field(
+            name=f"`{config.BOT_PREFIX}invite` :",
             value="  *To provide an invite link for a Discord server.*",
         )
-        embedHelp.add_field(
-            name="`~editsnipe` :",
+        help_embed.add_field(
+            name=f"`{config.BOT_PREFIX}editsnipe` :",
             value="*To show the last message edited by a user within a minute on the same channel.*",
         )
-        embedHelp.add_field(
-            name="`~dctimer <time>` :",
+        help_embed.add_field(
+            name=f"`{config.BOT_PREFIX}dctimer <time>` :",
             value="*To automatically disconnect you from the voice channel after the specified time delay.*",
         )
-        embedHelp.add_field(name="\u200B", value="\u200B")
-        embedHelp.add_field(
-            name="Support Server", value="*https://discord.gg/mXPGpTMsqS*", inline=False
-        )
-        embedHelp.add_field(
-            name="_Messages from Elimina are deleted after 1 minute in toggled on channels_",
-            value="*For help contact: eliminabot@gmail.com*",
+        help_embed.add_field(name="\u200B", value="\u200B")
+        help_embed.add_field(
+            name="Support Server",
+            value=f"*{config.SUPPORT_SERVER_INVITE}*",
             inline=False,
         )
-        embedHelp.set_author(
+        help_embed.add_field(
+            name="_Messages from Elimina are deleted after 1 minute in toggled on channels_",
+            value=f"*For help contact: {config.SUPPORT_EMAIL}*",
+            inline=False,
+        )
+        help_embed.set_author(
             name="Elimina Bot",
-            url="https://github.com/NamelessDevelopers/EliminaBot",
+            url=config.GITHUB_URL,
             icon_url=self.bot.user.avatar_url,
         )
-        embedHelp.set_footer(text="Requested by: " + ctx.message.author.name)
-        await ctx.send(embed=embedHelp)
+        help_embed.set_footer(text="Requested by: " + ctx.message.author.name)
+        await ctx.send(embed=help_embed)
 
     @commands.command(name="info", aliases=["about", "information"])
     async def info(self, ctx: commands.Context) -> None:
-        embedInfo = discord.Embed(
+        info_embed = discord.Embed(
             title="Server Setup Information",
-            description="Elimina is a bot that can delete messages"
-            " sent from bot users after X number of seconds in toggled on channels. "
-            "Messages can be deleted after 1 second to after 300 seconds (5 minutes)! Elimina "
-            " also provides a snipe feature showing the most recently deleted text or image message.\nUse `~help` for more information",
+            description="Elimina is a bot that can delete messages sent from bot users"
+            + " after X number of seconds in toggled on channels. Messages can be deleted"
+            + " after 1 second to after 300 seconds (5 minutes)! Elimina also provides a"
+            + " snipe feature showing the most recently deleted text or image message.\nUse"
+            + f"`{config.BOT_PREFIX}help` for more information",
             colour=COLORS["accent"],
         )
 
-        val = str(ctx.guild.id)
-        mentionStr = ""
-        for elem in DATA[val]:
-            try:
-                int(elem)
-            except:
-                continue
-            if elem != "" and elem != "\n" and int(elem) > 300:
-                try:
-                    mentionStr += ctx.guild.get_channel(int(elem)).mention + " "
-                except:
-                    continue
+        whitelists = await get_whitelists()
 
-        embedInfo.set_author(
+        guild_id = ctx.guild.id
+        guild = await get_guild(guild_id)
+        if not guild:
+            return
+        guild = guild[0]
+
+        mention_str = "  ".join(
+            [
+                ctx.guild.get_channel(int(channel_id)).mention
+                for channel_id in list(whitelists[guild_id]["channels"])
+            ]
+        )
+
+        info_embed.set_author(
             name="Elimina Bot",
-            url="https://github.com/NamelessDevelopers/EliminaBot",
+            url=config.GITHUB_URL,
             icon_url=self.bot.user.avatar_url,
         )
 
-        botStr = ""
-        for e in BOT[val]:
-            try:
-                int(e)
-            except:
-                continue
-            if e != "" and e != "\n" and int(e) > 300:
-                try:
-                    mem = await ctx.guild.fetch_member(int(e))
-                    botStr += mem.mention + " "
-                except:
-                    continue
+        bot_str = " ".join(
+            [f"<@{bot_id}>" for bot_id in list(whitelists[guild_id]["bots"])]
+        )
 
-        if mentionStr == "":
-            embedInfo.add_field(
+        if not len(mention_str):
+            info_embed.add_field(
                 name="Following channels are toggled on: ",
                 value="No Channels toggled on use ~toggle",
                 inline=False,
             )
         else:
-            embedInfo.add_field(
+            info_embed.add_field(
                 name="Following channels are toggled on: ",
-                value=mentionStr,
+                value=mention_str,
                 inline=False,
             )
 
-        if botStr == "":
-            embedInfo.add_field(
+        if not len(bot_str):
+            info_embed.add_field(
                 name="Following bots are ignored: ",
                 value="No bots are ignored ~ignore <@bot>",
                 inline=False,
             )
         else:
-            embedInfo.add_field(
-                name="Following bots are ignored: ", value=botStr, inline=False
+            info_embed.add_field(
+                name="Following bots are ignored: ", value=bot_str, inline=False
             )
 
-        if str(DATA[val][1]) == "0":
-            embedInfo.add_field(name="Image Snipe: ", value="disabled", inline=True)
-        else:
-            embedInfo.add_field(name="Image Snipe:  ", value="enabled", inline=True)
+        info_embed.add_field(
+            name="Image Snipe: ",
+            value="enabled" if guild.image_snipe else "disabled",
+            inline=True,
+        )
 
-        embedInfo.add_field(
-            name="Timer set at: __" + str(DATA[val][0]) + " seconds__",
+        info_embed.add_field(
+            name="Timer set at: __" + str(guild.delete_delay) + " seconds__",
             value="\u200b",
             inline=True,
         )
 
-        embedInfo.add_field(
-            name="Support Server", value="*https://discord.gg/mXPGpTMsqS*", inline=False
+        info_embed.add_field(
+            name="Support Server",
+            value=f"*{config.SUPPORT_SERVER_INVITE}*",
+            inline=False,
         )
-        embedInfo.set_footer(
+        info_embed.set_footer(
             text="Requested by: "
             + ctx.message.author.name
             + "\nAuthors: "
             + " ".join(author for author in SUPER_USERS.values())
         )
-        await ctx.send(embed=embedInfo)
+        await ctx.send(embed=info_embed)
 
     @commands.command(name="invite")
     async def invite(self, ctx: commands.Context) -> None:
         embed_invite = discord.Embed(
             title="Click here to invite me",
             colour=COLORS["accent"],
-            url="https://discord.com/api/oauth2/authorize?client_id=777575449957498890&permissions=17918992&scope=bot",
+            url=f"https://discord.com/api/oauth2/authorize?client_id={self.bot_id}&permissions=17918992&scope=bot",
         )
         embed_invite.add_field(
-            name="Support Server", value="*https://discord.gg/mXPGpTMsqS*", inline=False
+            name="Support Server",
+            value=f"*{config.SUPPORT_SERVER_INVITE}*",
+            inline=False,
         )
-        embed_invite.set_footer(text="\nFor help contact eliminabot@gmail.com")
+        embed_invite.set_footer(text=f"\nFor help contact {config.SUPPORT_EMAIL}")
         await ctx.send(embed=embed_invite)
 
     @commands.command(name="vote")
     async def vote(self, ctx: commands.Context) -> None:
-        voteEmbed = discord.Embed(
+        vote_embed = discord.Embed(
             title="Click here to vote for me",
             color=COLORS["accent"],
-            url="https://top.gg/bot/777575449957498890/vote",
+            url=f"https://top.gg/bot/{config.TOP_GG_ID}/vote",
         )
-        voteEmbed.add_field(
-            name="Support Server", value="*https://discord.gg/mXPGpTMsqS*", inline=False
+        vote_embed.add_field(
+            name="Support Server",
+            value=f"*{config.SUPPORT_SERVER_INVITE}*",
+            inline=False,
         )
-        voteEmbed.set_footer(text="\nFor help contact eliminabot@gmail.com")
-        return await ctx.send(embed=voteEmbed)
+        vote_embed.set_footer(text=f"\nFor help contact {config.SUPPORT_EMAIL}")
+        return await ctx.send(embed=vote_embed)
 
 
 def setup(bot: commands.Bot) -> None:
