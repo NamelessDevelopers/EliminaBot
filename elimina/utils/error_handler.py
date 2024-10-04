6b9bd386a79e9796c48bc825246b1edc1d1b0a9c
ayamdobhal <me@iamdobhal.dev>
2024-10-04 09:15:14 +0530

    refactor: error handler

--- elimina/utils/error_handler.py
+++ elimina/utils/error_handler.py
@@ -7,7 +7,12 @@ from elimina import LOGGER
 from elimina.constants import COLORS
 
 
-class Errors(commands.Cog):
+class ErrorHandler(commands.Cog):
+    """
+    The ErrorHandler Class.
+
+    Class to handle various errors from `discord.ext.commands` and custom errors.
+    """
 
     __cog_name__ = "Error Handler"
 
@@ -16,44 +21,43 @@ class Errors(commands.Cog):
 
     @commands.Cog.listener()
     async def on_command_error(self, ctx: commands.Context, error: Any) -> None:
-        # TODO: use match expression instead
         if isinstance(error, commands.BotMissingPermissions):
-            permErrorEmbed = Embed(
+            perm_error_embed = Embed(
                 title=None,
                 color=COLORS["red"],
                 description="❌ Looks like I'm missing `manage channels` and/or `move members` permissions.",
             )
-            return await ctx.send(embed=permErrorEmbed)
+            return await ctx.send(embed=perm_error_embed)
 
         if isinstance(error, commands.CommandOnCooldown):
             command = self.bot.get_command(ctx.command.name)
-            cooldownEmbed = Embed(
+            cooldown_error_embed = Embed(
                 title=None,
                 color=COLORS["red"],
                 description="❌ That command is on cooldown for {} seconds.".format(
                     int(command.get_cooldown_retry_after(ctx))
                 ),
             )
-            return await ctx.send(embed=cooldownEmbed)
+            return await ctx.send(embed=cooldown_error_embed)
 
         if isinstance(error, commands.UserNotFound):
-            userErrorEmbed = Embed(
+            user_error_embed = Embed(
                 title=None, color=COLORS["red"], description="❌ User not found."
             )
-            return await ctx.send(embed=userErrorEmbed)
+            return await ctx.send(embed=user_error_embed)
 
         if ctx.command.name == "timer":
-            timerErrorEmbed = Embed(
+            timer_error_embed = Embed(
                 title=None,
                 color=COLORS["red"],
                 description="❌ Invalid value for time. Must be between 1 and 300",
             )
-            return await ctx.send(embed=timerErrorEmbed)
+            return await ctx.send(embed=timer_error_embed)
 
         else:
             LOGGER.exception(error)
 
 
 def setup(bot: commands.Bot) -> None:
-    bot.add_cog(Errors(bot))
-    LOGGER.info("Cog Loaded: ", Errors.__cog_name__)
+    bot.add_cog(ErrorHandler(bot))
+    LOGGER.info("Cog Loaded: ", ErrorHandler.__cog_name__)
